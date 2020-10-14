736768c97a0a658c8c418af910ae5e9eb717c99f
ducceschioa <72261064+ducceschioa@users.noreply.github.com>
2020-10-14 14:05:21 +0200

    Cubs

---
+++ Assets/Visibilitat Variables/ScrCub.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/*------------------------------------------------------------------------
+ * Autor: Jordi Aguilera
+ * Data: 28/09/16
+ * Descripció: script creat per mostrar diferents tipus de visibilitat de
+ *             les variables
+ * -----------------------------------------------------------------------*/
+  
+    public class ScrCub : MonoBehaviour {
+    public float forsa = 10;            // -------------------  variabla pública   --------------------------
+    int salts=5;                        // -------------------  variable privada   --------------------------
+    public static int cubsAEscena = 0;  // -------------------  variable de classe --------------------------
+
+    void Start()
+    {
+        cubsAEscena++;                   // cada vegada que es crea un enemic, incrementem aquesta variable
+        forsa = Random.Range(50, 100);    // establim la força de l'enemic amb un valor entre 50 i 100
+        print("Hi ha " + cubsAEscena + " cubs. Aquest té una força = " + forsa);  // mostrem el nombre de cubs i la força d'aquest
+    }
+
+    // Quan el cub es destrueixi, que decrementi el nombre de cubs
+    // OnDestroy és un event: quelcom que passa que és detectble pel sistema
+    void OnDestroy()
+    {
+        cubsAEscena--;
+    }
+}
---
+++ Assets/Visibilitat Variables/ScrTest.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*------------------------------------------------------------------------
+ * Autor: Jordi Aguilera
+ * Data: 28/09/16
+ * Descripció: es fa servir juntament amb ScrCub per mostrar aspectes 
+ *             relacionatas amb la visivilitat de les variables. 
+ * Interacció:
+ *      - Cada cop que pitgem la tecla C generarà un cub amb posició i color
+ *        aleatori, i mostrarà informació particular del cub i general de la 
+ *        classe (nombre de cubs a escena)
+ *      - Cada cop que pitgem I, mostra el nombre de cubs que hi ha a l'escena, 
+ *        llegint la variable de classe públic cubsAEscena
+ *        
+ * Inicialització: 
+ *   Cal establir la variable pública cub a l'Inspector
+ *   
+ * Notes:
+ *   Podem esborrar cubs des del inspector, per veure com el nombre de cubs 
+ *   s'actualitza automàticament  
+ * -----------------------------------------------------------------------*/
+
+public class ScrTest : MonoBehaviour {
+
+    public GameObject cub;
+
+	// Update is called once per frame
+	void Update () {
+        GameObject nuevoCubo;
+        if (Input.GetKeyDown(KeyCode.C))  // Generem cubs
+        {
+            nuevoCubo = Instantiate(cub);                   // creem un nou cub
+            nuevoCubo.name = "Cub " + ScrCub.cubsAEscena;   // li donem un nom
+            // I el posicionem en una ubicació aleatoria entre -3 i 3 per x, y i z
+            nuevoCubo.transform.position = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+            nuevoCubo.GetComponent<Renderer>().material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+        
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            print("El nombre total de cubs és: "+ScrCub.cubsAEscena );
+        }
+
+        if (Input.GetMouseButtonDown(0)) Test(); // Si pitgem el ratolí, mirem si hem picat sobre algun cub
+    }
+
+    void Test()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);   
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+             print("El " + hit.transform.name + " té una força de: "+hit.transform.GetComponent<ScrCub>().forsa);
+        }
+    }
+}
